77155937e33bd722a62b6ec3d0ba9e14ce221fe1
Michael Babaev <mbabaev@stream.ru>
2019-03-07 21:06:22 +0300

    Pinterestable ProductCatalogCollectionView

---
+++ Swappy/Extensions/UICollectionView+RegisterCells.swift
@@ -0,0 +1,9 @@
+//
+//  UICollectionView+RegisterCells.swift
+//  Swappy
+//
+//  Created by Бабаев Михаил Михайлович on 07/03/2019.
+//  Copyright © 2019 SwappyTeam. All rights reserved.
+//
+
+import Foundation
