0876b12307b20d697f65fd54825f37786fd11879
Michael Babaev <mmbabaev@gmail.com>
2019-03-17 23:40:33 +0300

    add product

---
+++ Swappy/UI/AddProduct/AddProductPresenter.swift
@@ -0,0 +1,9 @@
+//
+//  AddProductPresenter.swift
+//  Swappy
+//
+//  Created by Mihail on 17/03/2019.
+//  Copyright © 2019 SwappyTeam. All rights reserved.
+//
+
+import Foundation
---
+++ Swappy/UI/AddProduct/AddProductViewController.swift
@@ -0,0 +1,11 @@
+//
+//  AddProductViewController.swift
+//  Swappy
+//
+//  Created by Mihail on 17/03/2019.
+//  Copyright © 2019 SwappyTeam. All rights reserved.
+//
+
+protocol AddProductView: class {
+    func showProduct(viewModel: )
+}
