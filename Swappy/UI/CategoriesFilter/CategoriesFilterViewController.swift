2ee10704e2fb8bf81fd861c6c3fc4733eb46090b
Michael Babaev <mmbabaev@gmail.com>
2019-05-12 13:27:30 +0300

    add cards SBs

---
+++ Swappy/UI/CategoriesFilter/CategoriesFilterViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CategoriesFilter.swift
+//  Swappy
+//
+//  Created by Mihail on 12/05/2019.
+//  Copyright © 2019 SwappyTeam. All rights reserved.
+//
+
+import Foundation
+
+final class CategoriesFilterViewController: CardViewController {
+    
+    // MARK: - Properties
+    
+    
+    
+    // MARK: - Functions Lifecycle
+    
+    // MARK: -
+}
---
+++ Swappy/UI/CategorySelection/CategorySelectionViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CategorySelectionViewController.swift
+//  Swappy
+//
+//  Created by Mihail on 12/05/2019.
+//  Copyright © 2019 SwappyTeam. All rights reserved.
+//
+
+import UIKit
+
+class CategorySelectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
