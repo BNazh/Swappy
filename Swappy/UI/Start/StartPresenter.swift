aa927d96d8718acb56d2fb02c53515866e192ea2
Michael Babaev <mmbabaev@gmail.com>
2019-05-22 10:00:23 +0300

    add  Start components implementation

--- Swappy/UI/Start/StartPresenter.swift
+++ Swappy/UI/Start/StartPresenter.swift
@@ -9,16 +9,40 @@
 import Foundation
 
 protocol StartPresenter: class {
-    
+    func loadStartData()
 }
 
 final class StartPresenterImp {
+    
+    // MARK: - Properties
     
     private unowned let view: StartView
     private let router: StartRouter
     private let categoryService: CategoryService
     
-    init() {
+    // MARK: - Init
+    
+    init(view: StartView, router: StartRouter, categoryService: CategoryService) {
+        self.view = view
+        self.router = router
+        self.categoryService = categoryService
+    }
+}
+
+extension StartPresenterImp: StartPresenter {
+    
+    func loadStartData() {
+        let group = DispatchGroup()
         
+        group.enter()
+        categoryService.updateCategoryList { result in
+            
+            group.leave()
+        }
+        
+        group.notify(queue: .main) { [weak self] in
+            self?.view.stopLoading()
+            self?.router.showMainScreen()
+        }
     }
 }
--- Swappy/UI/Start/StartViewController.swift
+++ Swappy/UI/Start/StartViewController.swift
@@ -19,12 +19,40 @@ final class StartViewController: UIViewController {
     
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
+    var presenter: StartPresenter!
 
     // MARK: - Functions
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        loadData()
+    }
+    
+    func loadData() {
         activityIndicator.startAnimating()
+        presenter.loadStartData()
+    }
+}
+
+extension StartViewController: StartView {
+    
+    func showError() {
+        let title = "Ошибка"
+        let message = "Не удалось загрузить данные, необходимые для начала работы в приложении."
+        let actionTitle = "Повторить"
+        
+        let repeateAction = UIAlertAction(title: actionTitle, style: .default) { [weak self] _ in
+            self?.loadData()
+        }
+        
+        let alertVC = UIAlertController(title: title, message: message, preferredStyle: .actionSheet)
+        alertVC.addAction(repeateAction)
+        
+        present(alertVC, animated: true, completion: nil)
+    }
+    
+    func stopLoading() {
+        activityIndicator.stopAnimating()
     }
 }
