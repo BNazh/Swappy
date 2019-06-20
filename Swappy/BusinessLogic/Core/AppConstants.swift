24d63878d3d8935b109cf33382240d3a16d8820a
Michael Babaev <mmbabaev@gmail.com>
2019-06-20 23:17:32 +0300

    Revert "Delete AppConstants.swift"
    This reverts commit 7206b6304ac8e713aa34b44e05caca5011e881c7.

---
+++ Swappy/BusinessLogic/Core/AppConstants.swift
@@ -0,0 +1,18 @@
+//
+//  AppConstants.swift
+//  Swappy
+//
+//  Created by Mihail on 01/04/2019.
+//  Copyright © 2019 SwappyTeam. All rights reserved.
+//
+
+import Foundation
+
+enum AppConstants {
+    
+    static let defaultCurrency = "₽"
+    
+    enum Keys {
+        static let appMetrica = "e5fa98a5-b630-47e9-ab2f-69ce2c46ef1e"
+    }
+}
