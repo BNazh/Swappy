47737c08317325904f0d752a15ffd490f5db844e
Michael Babaev <mmbabaev@gmail.com>
2019-07-04 14:51:15 +0300

    refactor

--- Swappy/UI/Common/ProductCell/ProductCollectionViewCell.swift
+++ Swappy/UI/Common/ProductCell/ProductCollectionViewCell.swift
@@ -20,12 +20,6 @@ final class ProductCollectionViewCell: UICollectionViewCell {
     
     // MARK - Lifecycle
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        
-        layer.cornerRadius = 16
-    }
-    
     override func prepareForReuse() {
         super.prepareForReuse()
         
@@ -48,17 +42,6 @@ final class ProductCollectionViewCell: UICollectionViewCell {
 private extension ProductCollectionViewCell {
     
     func setupShadows() {
-//        self.contentView.layer.cornerRadius = 2.0
-//        self.contentView.layer.borderWidth = 1.0
-//        self.contentView.layer.borderColor = UIColor.clear.cgColor
-//        self.contentView.layer.masksToBounds = true
-//
-//        self.layer.shadowColor = UIColor.black.cgColor
-//        self.layer.shadowOffset = CGSize(width: 0, height: 2.0)
-//        self.layer.shadowRadius = 2.0
-//        self.layer.shadowOpacity = 0.5
-//        self.layer.masksToBounds = false
-//        self.layer.shadowPath = UIBezierPath(roundedRect: self.bounds, cornerRadius: self.contentView.layer.cornerRadius).cgPath
         
         layer.cornerRadius = 16
         layer.masksToBounds = false
