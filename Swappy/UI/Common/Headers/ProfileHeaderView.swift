ff5ba1b09cb981e004ad927cf327493c045b4279
Michael Babaev <mmbabaev@gmail.com>
2019-07-04 19:35:58 +0300

    add header files

---
+++ Swappy/UI/Common/Headers/ProfileHeaderView.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileHeader.swift
+//  Swappy
+//
+//  Created by Mihail on 04/07/2019.
+//  Copyright © 2019 SwappyTeam. All rights reserved.
+//
+
+import UIKit
+
+final class ProfileHeaderView: UIView {
+    
+}
