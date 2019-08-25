40caa674555a862b9a4494735906c739dc1d5e64
Michael Babaev <mmbabaev@gmail.com>
2019-08-25 12:59:00 +0300

    implement FavoritesServiceImp

--- Swappy/BusinessLogic/Workers/Favorites/FavoritesServiceImp.swift
+++ Swappy/BusinessLogic/Workers/Favorites/FavoritesServiceImp.swift
@@ -6,16 +6,26 @@
 //  Copyright © 2019 SwappyTeam. All rights reserved.
 //
 
+import Moya
+
 final class FavoritesServiceImp {
     
     // MARK: - Properties
     
-    let notificationCenter: ProductsNotificationCenter
+    private let provider: BaseProvider<FavoritesTarget>
+    private let notificationCenter: ProductsNotificationCenter
+    private let keychainStore: KeychainStore
+    
+    private var currentRequests: [SetFavoriteRequest] = []
     
     // MARK: - Init
     
-    init(notificationCenter: ProductsNotificationCenter) {
+    init(provider: BaseProvider<FavoritesTarget>,
+         notificationCenter: ProductsNotificationCenter,
+         keychainStore: KeychainStore) {
+        self.provider = provider
         self.notificationCenter = notificationCenter
+        self.keychainStore = keychainStore
     }
 }
 
@@ -23,4 +33,54 @@ final class FavoritesServiceImp {
 
 extension FavoritesServiceImp: FavoritesService {
     
+    func getFavoriteProducts(callback: @escaping ResultCallback<[Product]>) {
+        
+    }
+    
+    func setFavorite(_ isFavorite: Bool, for productId: String, callback: @escaping ResultCallback<Void>) {
+        cancelRequestIfExist(with: productId)
+        
+        let target = setFavoriteTarget(isFavorite: isFavorite, productId: productId)
+        
+        let cancellable = provider.requestDecodable(target) { (result: Result<String?>) in
+            switch result {
+            case .success:
+                callback(.success)
+            case .failure(let error):
+                callback(.failure(error))
+            }
+        }
+        
+        let request = SetFavoriteRequest(productId: productId, cancellable: cancellable)
+        currentRequests.append(request)
+    }
+}
+
+// MARK: - Private
+
+private extension FavoritesServiceImp {
+    
+    // MARK: - Types
+    
+    struct SetFavoriteRequest {
+        let productId: String
+        let cancellable: Cancellable
+    }
+    
+    // MARK: - Functions
+    
+    func cancelRequestIfExist(with productId: String) {
+        let request = currentRequests.first { $0.productId == productId }
+        request?.cancellable.cancel()
+        currentRequests.removeAll(where: { $0.cancellable.isCancelled })
+    }
+    
+    func setFavoriteTarget(isFavorite: Bool, productId: String) -> FavoritesTarget {
+        let userId = keychainStore.userSellerId ?? ""
+        if isFavorite {
+            return .addFavorite(userId: userId, productId: productId)
+        } else {
+            return .deleteFavorite(userId: userId, productId: productId)
+        }
+    }
 }
